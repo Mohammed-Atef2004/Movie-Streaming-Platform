ebdbc30399853fbdbaca874241df36758f796684
rawan929 <rawanosama929@gmail.com>
2025-09-25 18:22:48 +0300

    Add files via upload

---
+++ BLL/Services/Abstraction/IBookmarkService.cs
@@ -0,0 +1,17 @@
+﻿using BLL.ViewModels;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.Abstraction
+{
+    public interface IBookmarkService
+    {
+        (bool, string) AddBookmark(BookmarkVM bookmarkVM);
+        (bool, string) RemoveBookmark(BookmarkVM bookmarkVM);
+        IEnumerable<Bookmark> GetUserBookmarks(int userId);
+    }
+}
