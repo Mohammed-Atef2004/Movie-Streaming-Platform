ce60ddaed0d6035b0de665ab383bdf24402ab3e7
rawan929 <rawanosama929@gmail.com>
2025-09-25 18:17:59 +0300

    Add files via upload

---
+++ DAL/Repositories/Abstraction/IBookmarkRepository.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Abstraction
+{
+    public interface IBookmarkRepository : IGenericRepository<Bookmark>
+    {
+        //bool Update(Bookmark bookmark);
+        IEnumerable<Bookmark> GetUserBookmarks(Bookmark bookmark);
+        bool AddBookmark(Bookmark bookmark);
+        bool RemoveBookmark(Bookmark bookmark);
+    }
+}
