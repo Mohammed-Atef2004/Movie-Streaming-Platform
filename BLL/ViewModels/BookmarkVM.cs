cc6e3b9fb24f2640ecc34dbe7b3cf6c02142443d
rawan929 <rawanosama929@gmail.com>
2025-09-25 18:16:18 +0300

    Add files via upload

---
+++ BLL/ViewModels/BookmarkVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ViewModels
+{
+    public class BookmarkVM
+    {
+        public int Id { get; set; }         
+        public int UserId { get; set; }    
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
